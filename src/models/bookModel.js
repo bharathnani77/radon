55d0af0a4cd8bf62210de36b02469527d35dd9a6
bharathnani77 GitHub <bharath.nani8@gmail.com>
2022-06-07 22:18:38 +0530

    without db server

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,16 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+    bookName: {
+        type: String,
+        requried: true
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    authorName: String,
+    categoryName:{
+        type: String,
+        enum: ["Encyclopedia","Fictional","Health and Fitness","Diary","science"]
+    },
+    year: Number
+    },{ timestamps: true });
 
-
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+module.exports = mongoose.model('Book', bookSchema) // Book=> books    
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
+const UserModel = require("../models/userModel")
 const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const bookModel = require("../models/bookModel")
+const bookController = require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
+
+router.get("/books", function (req, res) {
+    res.send("My first books api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createBook", bookController.createBook )
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getBooksData", bookController.getBooksData)
 
-router.post("/createBook", BookController.createBook  )
-
-router.get("/getBooksData", BookController.getBooksData)
 
 module.exports = router;
