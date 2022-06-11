1b3366ad853f9f8f2c27412dcffb0c3e843af0d5
bharathnani77 GitHub <bharath.nani8@gmail.com>
2022-06-11 23:08:21 +0530

    assignUpdated

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
+const {timeStamp} = require ('console');
+const moment = require ('moment')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
 app.use (
     function (req, res, next) {
+        const today = moment();
+        console.log(today.format('YYYY-MM-DD HH:MM:SS')+ ","+req.ip+","+req.path);
         console.log ("inside GLOBAL MW");
         next();
   }
   );
 
-app.use('/', route);
-
-
+  app.use('/',route)
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,26 +1,24 @@
-
-const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
+const midGlb = function (req,res,next){
+    console.log("Hi im a global middleware");
     next()
 }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
+const assignmentMW = function (req,res,next){
+    let currentDate = new Date();
+    let dateTime = currentDate.getDate() + " " + (currentDate.getMonth()+1) + " " + currentDate.getFullYear() + " "
+                    + currentDate.getHours() + " " + currentDate.getMinutes() + " " + currentDate.getSeconds();
+
+    let ip = req.ip
+    let url = req.originalUrl
+
+    console.log('${dateTime} ${ip} ${url}')
+
     next()
 }
 
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
-}
+app.use(assignmentMW)
 
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
+module.exports.midGlb= midGlb
+module.exports.assignmentMW = assignmentMW
 
-module.exports.mid1= mid1
-module.exports.mid2= mid2
-module.exports.mid3= mid3
-module.exports.mid4= mid4
+
